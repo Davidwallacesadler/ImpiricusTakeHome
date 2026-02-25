ad59b5ff002685169331e73cfc39f4502773234e
David Sadler <davidwallacesadler@gmail.com>
2026-02-25 07:22:02 -0700

    Initial Commit

---
+++ ImpiricusTakeHome/ImpiricusTakeHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImpiricusTakeHomeApp.swift
+//  ImpiricusTakeHome
+//
+//  Created by David Sadler on 2/25/26.
+//
+
+import SwiftUI
+
+@main
+struct ImpiricusTakeHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
